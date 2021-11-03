d835f4527e4881c9c1719c23651df23051d02b59
unknown <justiceibiam@gmail.com>
2021-11-03 19:18:45 +0100

    added the action drop down menu

--- resources/views/vehicles.blade.php
+++ resources/views/vehicles.blade.php
@@ -69,6 +69,10 @@
 
                                             <td>
                                                 <div class="btn-group">
+                                                    <button type="button" class="btn btn-dark btn-sm dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-reference="parent">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+                                                    </button>
+                                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuReference1">
                                                     <a class="dropdown-item" href="{{ route('vehicles.edit',$vehicle->id) }}">Edit</a>
                                                     <a class="dropdown-item" href="{{ route('vehicles',$vehicle->id) }}">View</a>
 
@@ -77,7 +81,7 @@
                                                             @method("DELETE")
                                                             <input class="form-item" class="dropdown-item" value="Delete" type="submit"/>
                                                         </form>
-
+                                                    </div>
                                                     </div>
                                                 </div>
                                             </td>
