65b7dc7d4ea31558ef6fe4aa6dcb81cfbf860e3b
= <idionomfon.etim@dreamlabs.com.ng>
2021-11-03 12:21:24 +0100

    modify invoice table and update invoice generation module

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -66,12 +66,15 @@ class InvoiceController extends Controller
             'owner_name' => "required|string|max:255",
             "owner_email" => "required|string|email|max:255",
             "owner_phone"=> "required|string|max:11",
-            "engine_number"=> "required|string",
+            "engine_number"=> "sometimes|string",
             "license_number"=> "required|string",
             "plate_number" => "required|string",
-            "chassis_number"=> "required|string",
+            "chassis_number"=> "sometimes|string",
             "vehicle_type"=> "required|integer"
         ]);
+
+        // dd(config('REVPRO_STAGING_URL '));
+        $link = "https://staging.revenue.ng/api/v1/autoRegInvoice?token=455UI345GNTF95DFJC82BS0995BNF898FHFBHF21213HFHRH2HHD8FBNBDGFSWCVBDJ&userId=54";
         //    Use the plate_number and retrieve the vehicle
         $query = Vehicle::where("plate_number", $request->plate_number);
 
@@ -89,31 +92,42 @@ class InvoiceController extends Controller
                 $selected_services = $request->service;
                 
                 foreach ($selected_services as $servId) {
+                    
                     $query = Service::where("id",  $servId);
+                    
                     if(!$query->exists()){
                         // Service Ids' tampered with.
                         return back()->with("error". "Error occured. Kindly try again");
                     }
+                    
                     $service = $query->select('service_name', 'description', 'item_code')->first();
                     
                    if(!$request->has("service_{$servId}") && count($request->input("service_{$servId}")) == 0){
                         return back()->with("error", "Service component not selected");
                    }
-                    $selected_components = $request->input("service_{$servId}");
+                    
+                   $selected_components = $request->input("service_{$servId}");
 
                     $comp_amts = 0; //track total service amounts base on each component array;
                     
                     foreach ($selected_components as $cmpId) {
                         $query = ServiceComponent::where("id", $cmpId)->where("service_id",  $servId);
+                        
                         if(!$query->exists()){
                             // Component Id's tampered with from the front-end
                             return back()->with("error", "Error occured, please try again");
                         }
-                        $comp_amts += $query->value("amount");
 
+                        $queryRes = $query->select('amount', 'title')->first();
+                        
+                        $comp_amts += $queryRes->amount;
                         // prepare invoice breakdown records to be inserted
-                        array_push($services_comp, ["service_id"=> $servId, "component_id"=> $cmpId]);
-                   }//EndForeach Components
+
+                        // Generate a mapping of service component to the different service Ids
+                        // This is will latter help while inserting the different service components as invoice breakdown.
+                        $services_comp["{$servId}"][] = ["service_id"=> $servId, "component_id"=> $cmpId, "title"=>  $queryRes->title, "amount"=> $queryRes->amount];
+                        
+                   }//end components foreach
 
                 // Prepare invoice records to be inserted
                    
@@ -123,12 +137,9 @@ class InvoiceController extends Controller
                     "amount"=> $comp_amts, 
                     "service_id"=> $servId, 
                     "status"=> "unpaid",
-                    "mnta_ref" => "mnta_65756hrtyr",
-                    "file" => "http/data.url",
                 ]);
 
                 // prepare payload's service data
-                
                 $serviceObj = (object) array(
                     "name"=> $service->service_name, 
                     "amount"=> $comp_amts, 
@@ -138,27 +149,8 @@ class InvoiceController extends Controller
 
                 array_push($payload_services, $serviceObj);
 
-            }//EndForeach Services
+            }//end services foreach
 
-            // Store invoice and invoice breakdown
-            foreach ($invoiceQueryData as $data) {
-                $invoice_array = [];
-                
-                $id = DB::table('invoices')->insertGetId($data);
-                
-                $invoice_array["invoice_id"] = $id;
-                
-                $invoice_array["service_id"] = $data["service_id"];
-
-                foreach ($services_comp as $component) {
-                  if($component["service_id"] === $invoice_array["service_id"]){
-                    // Track the invoice Id also
-                    $component["invoice_id"] = $invoice_array["invoice_id"];
-                    DB::table('invoices_breakdown')->insert($component);
-                  }
-                }
-            }
-           
             // Prepare the payload's data
             $payload = (object) array(
                 "first_name" => $vehicle->owner_fname,
@@ -167,19 +159,19 @@ class InvoiceController extends Controller
                 "phone" => $vehicle->owner_phone,
                 "tin" => "2343253534", // query here for more info
                 "birs_id"=> "3423424", // query here for more info
-                "payment_type"=> "bank", //query here for more info,
+                "payment_type"=> "bank", 
                 "service" => $payload_services,
-                "engine_number"=> $vehicle->engine_number,
-                "chassis_number"=> $vehicle->chassis_number,
-                "plate_number" => $vehicle->plate_number,
-                "model" => $vehicle->model,
+                "engine_number"=> $vehicle->engine_number ?? "",
+                "chassis_number"=> $vehicle->chassis_number ?? "",
+                "plate_number" => $vehicle->plate_number ?? "",
+                "model" => $vehicle->model ?? "",
                 "category"=> $vehicle->category,
                 "type" => VehicleType::where("id", $request->vehicle_type)->value("title"), //query here for more info
-                "policy_number" => $vehicle->policy_number,
-                "engine_capacity"=> $vehicle->engine_capacity,
-                "odometer" => $vehicle->odometer,
-                "color"=> $vehicle->color,
-                "fuel_type" => $vehicle->fuel_type,
+                "policy_number" => $vehicle->policy_number ?? "",
+                "engine_capacity"=> $vehicle->engine_capacity ?? "",
+                "odometer" => $vehicle->odometer ?? "",
+                "color"=> $vehicle->color ?? "",
+                "fuel_type" => $vehicle->fuel_type ?? "",
                 "year_of_manufacture" => $vehicle->year_of_manufacture,
                 "title"=> $vehicle->title,
                 "address" => $vehicle->address,
@@ -190,57 +182,82 @@ class InvoiceController extends Controller
                 "lga" => $vehicle->lga,
 
             );
-
-
-            // dd(json_encode($payload));
-            $pdf = App::make('dompdf.wrapper');
+           
+            $curl = curl_init();
             
-            $pdf->loadView('pdf.invoice');
-
-            // $pdf = PDF::loadView('admin.tagihan.pdf', compact('bills','today')); passing data to view here
-
-            $content = $pdf->download()->getOriginalContent();
-
-            $filename = uniqid().".pdf";
-            
-            Storage::disk('local')->put("/invoices/{$filename}",  $content);
-
-            // Store Files in the session and redirect to render data
-
-            // Move file 
-
-
-            // Interact with Revpro's API
-        //     $curl = curl_init();
-            
-        //     curl_setopt_array($curl, array(
-        //         CURLOPT_URL => env("REVPRO_API_URL"),
-        //         CURLOPT_RETURNTRANSFER => true,
-        //         CURLOPT_HTTPHEADER => [
-        //             "accept: application/json",
-        //             "authorization: Bearer " ."123456789098tyy",
-        //             "cache-control: no-cache"
-        //         ],
-        //         CURLOPT_POST => 1,
-        //         CURLOPT_POSTFIELDS => json_encode( $payload),
-        //     ));
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $link,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HTTPHEADER => [
+                    "accept: application/json",
+                    "cache-control: no-cache"
+                ],
+                CURLOPT_POST => 1,
+                CURLOPT_POSTFIELDS => json_encode($payload),
+            ));
          
-        //     $response = curl_exec($curl);
+            $response = curl_exec($curl);
                  
-        //     $err = curl_error($curl);
+            $err = curl_error($curl);
+
+            curl_close($curl);
          
-        //     if ($err) {
-        //         // there was an error interacting with Reprov's  API
-        //         // die('Curl returned error: ' . $err);
-        //         return back()->with("error", "Error occured while initiating request. Please try again later");
-        //     }
+            if ($err) {
+                // there was an error while interacting with Reprov's  API
+                return back()->with("error", "Error occured while initiating request. Please try again later. Check your internet connection.");
+            }
+            
+            $response = json_decode($response);
+            
+            if($response->status !== "success" || empty($response->invoice_no)){
+                return back()->with("error", "Error occured while initiating request. Please try again later");
+            }
 
-        //    if($response->status){
-        //     //  do something here when done
-        //    }
+            // continue the process. Invoice generated on Repro's end
+             // Store invoice and invoice breakdown
+            foreach ($invoiceQueryData as $key => $data) {
 
-            // curl_close($ch);
+                $data["trans_ref"] = $response->data[$key]->trans_reference;
 
+                $data["created_at"] = now();
+
+                // Retrieve service's breakdownn using the different service Ids
+                $invoice_breakdown = $services_comp["{$data["service_id"]}"];
+
+                // Generate invoice PDF
+                $pdf = App::make('dompdf.wrapper');
+            
+                $pdf->loadView('pdf.invoice', ["vehicle"=>$vehicle, "invoice_breakdown"=> $invoice_breakdown, "invoice" => ["date_created"=> $data["created_at"], "trans_ref"=> $data["trans_ref"], "agent"=> "Ada Jacobs", "status"=> "Unpaid", "service"=> $response->data[$key]->name, "total_amount"=>$response->data[$key]->amount]]);
+              
+                $content = $pdf->download()->getOriginalContent();
+
+                $filename = uniqid().".pdf";
+
+                // Upload file to invoice folder
+                Storage::disk('local')->put("/invoices/{$filename}",  $content);
+
+                $data["file"] = Storage::url("invoices/{$filename}");
+
+                $data["created_by"] = auth()->user()->id;
+                
+                // Store Invoice
+                $id = DB::table('invoices')->insertGetId($data);
+
+                // Retrieve and format invoice breakdown
+                $invoice_breakdown = array_map(function($data) use ($id) {
+                    $data["invoice_id"] = $id;
+                    unset($data["title"]);
+                    unset($data["amount"]);
+                    return $data;
+                }, $invoice_breakdown);
+
+                //  Finally, store invoice breakdown
+                DB::table('invoices_breakdown')->insert($invoice_breakdown);
+               
+            }
+
+            // Do something here
+            return back()->with("success", "Vehicle renewal initiated successfully");
         
         }
 
