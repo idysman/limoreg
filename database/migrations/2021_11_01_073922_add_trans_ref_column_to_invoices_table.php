78b1560a14d1994cb0e9bafe697d2a3b14d0f3ae
unknown <justiceibiam@gmail.com>
2021-11-05 08:38:53 +0100

    updated migration file

--- database/migrations/2021_11_01_073922_add_trans_ref_column_to_invoices_table.php
+++ database/migrations/2021_11_01_073922_add_trans_ref_column_to_invoices_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddFileAndTransRefColumnToInvoicesTable extends Migration
+class AddTransRefColumnToInvoicesTable extends Migration
 {
     /**
      * Run the migrations.
