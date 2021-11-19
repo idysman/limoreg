f1414a8e0d013c4dc4bf96fcc9517152d81d6bab
unknown <justiceibiam@gmail.com>
2021-11-19 08:12:56 +0100

    edited footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
         <!--  END CONTENT AREA  -->
 <div class="footer-wrapper">
     <div class="footer-section f-section-1">
-        {{-- <p class="" style="padding-left: 140px;">Copyright © {{ date('Y') }}. <a target="_blank" href="https://dreamlabs.com.ng">Powered by Dreamlabs Nigeria Limited</a>, All rights reserved.</p> --}}
+        <p class="" style="padding-left: 140px;">All right reserved @Imo State Internal Revenue Service (IIRS)</p>
     </div>
 </div>
 
