d1d5e0ecefe78d42562c6ead80f7d10d9230828a
unknown <justiceibiam@gmail.com>
2021-11-19 08:02:38 +0100

    minor changes

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
         <!--  END CONTENT AREA  -->
 <div class="footer-wrapper">
     <div class="footer-section f-section-1">
-        <p class="" style="padding-left: 140px;">Copyright © {{ date('Y') }}. <a target="_blank" href="https://dreamlabs.com.ng">Powered by Dreamlabs Nigeria Limited</a>, All rights reserved.</p>
+        {{-- <p class="" style="padding-left: 140px;">Copyright © {{ date('Y') }}. <a target="_blank" href="https://dreamlabs.com.ng">Powered by Dreamlabs Nigeria Limited</a>, All rights reserved.</p> --}}
     </div>
 </div>
 
